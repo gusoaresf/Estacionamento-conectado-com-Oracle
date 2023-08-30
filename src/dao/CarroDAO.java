0f6f4d9db888f59fe4ea564770eb65179252c049
Gustavo Soares <guusoaresss@icloud.com>
2023-08-30 09:48:32 -0300

    first commit

---
+++ src/dao/CarroDAO.java
@@ -0,0 +1,40 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import bean.carro;
+
+public class CarroDAO {
+    
+    private Connection con;
+
+    public Connection getCon() {
+        return con;
+    }
+
+    public void setCon(Connection con) {
+        this.con = con;
+    }
+
+    public CarroDAO(Connection con) {
+        this.con = con;
+    }
+
+    public String inserir(carro carro){
+        String sql = "insert into carro(placa,cor,descricao) values(?,?,?)";
+        try{
+            PreparedStatement ps = getCon().prepareStatement(sql);
+            ps.setString(1, carro.getPlaca());
+            ps.setString(2, carro.getCor());
+            ps.setString(3, carro.getDescricao());
+            if (ps.executeUpdate() > 0){
+                return "inserido com sucesso";
+            } else {
+                return "Erro ao inserir";
+            }
+        } catch (SQLException e){
+            return e.getMessage();
+        }
+    }
+}
---
+++ src/dao/Conexcao.java
@@ -0,0 +1,41 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexcao {
+
+    public static Connection abrirConexcao(){
+        Connection con = null;
+        try{
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            String url = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL";
+            final String USER = "XXX";
+            final String PASS = "XXX";
+            con = DriverManager.getConnection(url, USER, PASS);
+            System.out.println("Conexao aberta");
+
+        } catch (ClassNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+        catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        catch (Exception e){
+            System.out.print(e.getMessage());
+        }
+        return con;
+    }
+
+    public static void fecharConexcao(Connection con){
+        try{
+            con.close();
+            System.out.println("Conexcao fechada");
+        } catch (SQLException e){
+            System.out.println(e.getMessage());
+        } catch (Exception e){
+            System.out.println(e.getLocalizedMessage());
+        }
+    }
+}
