fcba5a71fbf9cac39cabf599bc227b543b15be1d
Gustavo Soares <guusoaresss@icloud.com>
2023-09-01 12:00:40 -0300

    teste 4 finalizado

--- src/main/Teste4.java
+++ src/main/Teste4.java
@@ -1,14 +1,26 @@
 package main;
 
 import java.sql.Connection;
+import java.util.ArrayList;
 
+import bean.carro;
+import dao.CarroDAO;
 import dao.Conexcao;
 
 public class Teste4 {
     public static void main(String[] args) {
         Connection con = Conexcao.abrirConexcao();
 
-        
+        CarroDAO cd = new CarroDAO(con);
+        ArrayList<carro> lista = cd.listarTodos();
+
+        if (lista !=null) {
+            for (carro carro : lista) {
+                System.out.println("Placa: " + carro.getPlaca());
+                System.out.println("Cor: " + carro.getCor());
+                System.out.println("Descrição: " + carro.getDescricao());
+            }
+        }
 
         Conexcao.fecharConexcao(con);
     }
