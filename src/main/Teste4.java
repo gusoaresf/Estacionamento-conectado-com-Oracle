eeb3a3507b326d7fb569ee641c384f31206e65c1
Gustavo Soares <guusoaresss@icloud.com>
2023-09-01 11:56:07 -0300

    Connection

--- src/main/Teste4.java
+++ src/main/Teste4.java
@@ -1,7 +1,15 @@
 package main;
 
+import java.sql.Connection;
+
+import dao.Conexcao;
+
 public class Teste4 {
     public static void main(String[] args) {
+        Connection con = Conexcao.abrirConexcao();
+
         
+
+        Conexcao.fecharConexcao(con);
     }
 }
